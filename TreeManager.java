6fe6709108da5f6216c616b7162e058c5c878280
Cohav0310 <cohav0310@gmail.com>
2024-11-22 09:04:26 -0800

    Adding classes
    classes have been added with more work started on TreeNode class and Manager class.

---
+++ TreeManager.java
@@ -0,0 +1,14 @@
+public class TreeManager {
+    private TreeNode current = null;
+    public InsertNode(String name){
+        if(root == null){
+            root = new TreeNode(element);
+        }
+        else{
+            current = root;
+            while(current != null){
+                //if(name < current.element)
+            }
+        }
+    }
+}
